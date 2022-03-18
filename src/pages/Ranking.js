93fc67ca335b398e2b3a6497536185a56bc5bb8c
Brenno Miliani <brennomiliani@hotmail.com>
2022-03-18 15:57:57 -0300

    ranking style

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -30,23 +30,40 @@ class Ranking extends Component {
     }).map((player, index) => {
       const imgUrl = playerImgRequest(player.picture);
       return (
-        <div key={ index }>
-          <p data-testid={ `player-name-${index}` }>{ player.name }</p>
-          <p data_testid={ `player-score-${index}` }>{ player.score }</p>
-          <img className="rounded" src={ imgUrl } alt={ player.name } />
+        <div className="mt-5 d-flex flex-column align-items-center" key={ index }>
+          <h3 data-testid={ `player-name-${index}` }>
+            {index === 0 && <span role="img" aria-label="emoji">🥇</span>}
+            {index === 1 && <span role="img" aria-label="emoji">🥈</span>}
+            {index === 2 && <span role="img" aria-label="emoji">🥉</span>}
+            { player.name }
+          </h3>
+          <p data_testid={ `player-score-${index}` }>
+            Pontos:
+            {' '}
+            { player.score }
+          </p>
+          <img className="rounded w-100" src={ imgUrl } alt={ player.name } />
         </div>
       );
     });
 
     return (
-      <div>
-        <h2 data-testid="ranking-title">Ranking</h2>
-        <div>
+      <div className="d-flex flex-column align-items-center my-5">
+        <h2
+          className="mb-5"
+          data-testid="ranking-title"
+        >
+          Ranking
+
+        </h2>
+        <div className="w-25 mx-auto d-flex flex-column align-items-center">
           { rankingOrder }
         </div>
 
         <button
+          className="btn btn-primary mt-5 w-25"
           type="button"
+          medal
           data-testid="btn-go-home"
           onClick={ () => history.push('/') }
         >
